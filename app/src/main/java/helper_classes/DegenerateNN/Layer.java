2b6f9f480d623f2027619c811b0b8514faca43a9
hi-brylle <kiirbritmiin@gmail.com>
2019-11-13 00:21:04 +0800

    put some guts in the Layer class

--- app/src/main/java/helper_classes/DegenerateNN/Layer.java
+++ app/src/main/java/helper_classes/DegenerateNN/Layer.java
@@ -1,4 +1,46 @@
 package helper_classes.DegenerateNN;
 
 public class Layer {
+    private Matrix weightedInputs;
+    private Matrix activations;
+    private Matrix errors;
+    private double layerBias;
+
+    public Layer(){}
+
+    public void setLayerSize(int numberNodes, double bias){
+        weightedInputs.setDimensions(numberNodes, 1);
+        activations.setDimensions(numberNodes, 1);
+        errors.setDimensions(numberNodes, 1);
+        layerBias = bias;
+    }
+    public int getLayerSize(){
+        return activations.getRows();
+    }
+
+    public void setActivations(Matrix activations){
+        this.activations.copyFrom(activations);
+    }
+    public void setWeightedInputs(Matrix weightedInputs){
+        this.weightedInputs.copyFrom(weightedInputs);
+    }
+    public void setErrors(Matrix errors){
+        this.errors.copyFrom(errors);
+    }
+
+    public Matrix getWeightedInputs() {
+        return weightedInputs;
+    }
+
+    public Matrix getActivations() {
+        return activations;
+    }
+
+    public Matrix getErrors() {
+        return errors;
+    }
+
+    public double getLayerBias() {
+        return layerBias;
+    }
 }
---
+++ app/src/main/java/helper_classes/DegenerateNN/Matrix.java
@@ -0,0 +1,35 @@
+package helper_classes.DegenerateNN;
+
+class Matrix {
+    private int rows;
+    private int columns;
+    private double[][] data;
+
+    int getRows() {
+        return rows;
+    }
+
+    int getColumns(){
+        return columns;
+    }
+
+    double getDataAt(int i, int j){
+        return data[i][j];
+    }
+
+    void setDimensions(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+
+        data = new double[this.rows][this.columns];
+    }
+
+    void copyFrom(Matrix source) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                data[i][j] = source.getDataAt(i,j);
+            }
+        }
+    }
+
+}
